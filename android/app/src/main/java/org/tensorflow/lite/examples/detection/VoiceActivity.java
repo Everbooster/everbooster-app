a6e74637fc06c51d1049bd02cd4296041cf90b7f
DESKTOP-OJQQ6HL\Scorpion <scorpion@dgu.ac.kr>
2021-09-26 13:40:28 +0900

    voice routines are regularized and time duration is set

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/VoiceActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/VoiceActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +12,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.lang.*;
 
 public class VoiceActivity extends AppCompatActivity {
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-
+        Handler handler = new Handler();
         String title = intent.getStringExtra("title"); //인식한 객체의 레이블을 받아오기
 
         MediaPlayer mediaPlayer;
@@ -26,42 +30,65 @@ public class VoiceActivity extends AppCompatActivity {
 
 
         if (title.equals("plastic bottle")) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.audio_1_pb);
-            mediaPlayer.start();
+            voiceGuidance(R.raw.audio_1_pb);
+            TimerSetting(15500,handler,newintent);
 
         } else if (title.equals("plastic")) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.audio_5_plastic);
-            mediaPlayer.start();
+            voiceGuidance(R.raw.audio_5_plastic);
+            TimerSetting(13500,handler,newintent);
 
         } else if (title.equals("can")) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.audio_4_can);
-            mediaPlayer.start();
+            voiceGuidance(R.raw.audio_4_can);
+            TimerSetting(17500,handler,newintent);
 
         } else if (title.equals("glass bottle")) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.audio_2_glass);
-            mediaPlayer.start();
+            voiceGuidance(R.raw.audio_2_glass);
+            TimerSetting(19500,handler,newintent);
 
         } else if (title.equals("plastic bag")) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.audio_6_pb);
-            mediaPlayer.start();
+            voiceGuidance(R.raw.audio_6_pb);
+            TimerSetting(13500,handler,newintent);
 
         } else if (title.equals("paper bag")) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.audio_3_paper);
-            mediaPlayer.start();
+            voiceGuidance(R.raw.audio_3_paper);
+            TimerSetting(15500,handler,newintent);
         }
 
-        //해당 객체에 대한 음성 안내가 끝나면 restart 안내 보이스 출력
-        mediaPlayer=MediaPlayer.create(this,R.raw.audio_7_restart);
-        mediaPlayer.start();
+//        //해당 객체에 대한 음성 안내가 끝나면 restart 안내 보이스 출력
+//        mediaPlayer=MediaPlayer.create(this,R.raw.audio_7_restart);
+//        mediaPlayer.start();
 
-        Handler handler = new Handler();
+
+
+    }
+
+    private void voiceGuidance(int mediaUid){
+        MediaPlayer mediaPlayer;
+        //해당 객체에 대한 음성 안내가 끝나면 restart 안내 보이스 출력
+        mediaPlayer=MediaPlayer.create(this,mediaUid);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                performOnEnd();
+            }
+        });
+        mediaPlayer.start();
+    }
+
+    private void performOnEnd(){
+        MediaPlayer mp;
+        //해당 객체에 대한 음성 안내가 끝나면 restart 안내 보이스 출력
+        mp=MediaPlayer.create(this,R.raw.audio_7_restart);
+        mp.start();
+    }
+    private void TimerSetting(int time, Handler handler, Intent nextintent){
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Log.e("Splash Activity", "Application is Running");
-                startActivity(newintent);
+                startActivity(nextintent);
                 finish();
             }
-        }, 20000);
+        }, time);
     }
 }
